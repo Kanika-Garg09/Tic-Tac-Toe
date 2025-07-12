bc7876ccd4d16e727426cd090d884a6946b74774
kanika garg <gargkanika115@gmail.com>
2025-07-12 19:42:47 +0530

    adding few lines of js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("welcome to Tic Tac Toe")
-let audioturn = new Audio("turnsound.mp3")
-let gaemover = new Audio("Tic-Tac-Toe/8-bit-game-over-sound-effect-331435.mp3")
-let turn ="X"                  /*yani intital turn x hai*/
+let audioturn = new Audio("turnsound.mp3")            //new Audio is used to add sound in js
+let gameover = new Audio("gameend.mp3")
+let turn ="X"                  //initial its x turn
 
 //function to change the turn
 
-const changeturn = ()=>{
-    return turn === "X"?"0": "X";
+const changeturn = ()=>{                     //its a arrow func ; we will create arror func in js 
+    return turn === "X"?"O": "X";            //agr x ka turn tha to ab 0 ka hai
 }
 
 //function to check win 
@@ -15,14 +15,14 @@ const checkwin=()=>{}
 
 //game logic
 
-let boxes = document.getElementsByClassName("box");
+let boxes = document.getElementsByClassName("box");     //it select all the elements of having class box and un sare elements ko "boxes" naam ke variable me store krega
 
-Array.from(boxes).forEach(element => {
-    let boxtext = element.querySelector('.boxtext');
+Array.from(boxes).forEach(element => {              
+    let boxtext = element.querySelector('.boxtext');     // ye box ke andr X ya O likhega
     
-    element.addEventListener('click', () => {
+    element.addEventListener('click', () => {                //jab us elements pe click kre yani x ya O pe click kre to niche dia hua kaam krna hai
         if (boxtext.innerText === '') {
-            boxtext.innerText = turn;
+            boxtext.innerText = turn;      
              audioturn.currentTime = 0;
             audioturn.play();
             turn = changeturn();
